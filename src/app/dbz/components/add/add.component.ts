93b9808823fda3c01030517453babd7e0bc5b76d
Aghent47 <neiderkk@gmail.com>
2024-12-12 10:58:01 -0500

    @Output() - Emitir eventos al padre

--- src/app/dbz/components/add/add.component.ts
+++ src/app/dbz/components/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/characters.interface';
 
 @Component({
@@ -10,6 +10,9 @@ import { Character } from '../../interfaces/characters.interface';
 })
 export class AddComponent {
 
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
   public character: Character =  {
     name: '',
     power: 0,
@@ -17,6 +20,14 @@ export class AddComponent {
 
   emitCharacter():void{
     console.log(this.character)
+    if(this.character.name.length === 0) return;
+
+    this.onNewCharacter.emit(this.character); // emitir el objeto ingresado 
+
+  //limpiar los inputs
+  this.character.name = '';
+  this.character.power = 0;
+
   }
 
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -21,5 +21,8 @@ export class DBZComponent{
   }
 ];
 
+  onNewCaracter(character:Character):void{
+    console.log(character);
+  }
 
 }
